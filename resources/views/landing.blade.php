65c4f3d5ecb5e2c8b0883670ecdc76804fe60e10
Elizabeth <eli456pg@gmail.com>
2023-11-17 15:55:35 -0600

    first commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="shortcut icon" href="logo.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <title>Flores</title>
+    <!-- CSS Only-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <img src="logo.png" width="100" height="100" float="right" />
+    <header
+        style="background-image: url('header.png'); background-size: contain; background-repeat: no-repeat; background-position: center center; height: 300px;">
+        <!-- Contenido de tu encabezado -->
+    </header>
+    <h1 class="mt-2" align="center" style="color: rgb(144, 12, 63);">Una flor florece para su propia alegria</h1>
+    <div class="row justify-content-end">
+        <div class="col-auto">
+            <a href="#1" class="btn btn-warning">Función</a>
+            <a href="#2" class="btn btn-warning">Tipos</a>
+            <a href="#3" class="btn btn-warning">Medicinales</a>
+            <a href="#4" class="btn btn-warning">¿Sabías que...?</a>
+        </div>
+    </div>
+</head>
+
+<body class="container" style="background-color: 
+#F9D2F3;"></body>
+<a id="1"></a>
+<h1>Principal función</h1>
+<p><B>Polinización </B> </br>
+    La polinización es un proceso fundamental en la reproducción de las plantas con flores (angiospermas). Implica el
+    traslado de polen, que contiene los gametos masculinos de la planta, desde el órgano reproductor masculino
+    (generalmente los estambres) de una flor a la parte receptiva del órgano reproductor femenino (generalmente el
+    pistilo) de otra flor o de la misma flor. Este proceso es esencial para que las plantas produzcan semillas y, en
+    muchos casos, frutos.
+</p>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <img src="https://s1.significados.com/foto/flor-polinizacion.jpg" width="300" height="190">
+        </div>
+        <div class="col-sm-8">
+            <h5>Características:</h5>
+            <p><I>Las flores tienen características funcionales y estéticas que las hacen reconocibles a simple vista,
+                    tales como:</I></br>
+                <li> Estructura o colores llamativos</li>
+                <li> Producción de nectar</li>
+                <li> Mecanismo de defensa de las plantas</li>
+        </div>
+    </div>
+</div>
+<br>
+<a id="2"></a>
+<h1>Las más conocidas </h1>
+<I>Plantas de desierto, flores estacionales etc.
+    Veamos algunas de las flores más conocidas y representativas
+    a continuación.
+    <br>
+    <div class="row">
+        <div class="col">
+            <img src="/resources/img/roses.jpg" width="280" height="190" />
+            <p>Rosas</p>
+        </div>
+        <div class="col">
+            <img src="peonias.jpg" width="280" height="190" />
+            <p>Peonías</p>
+        </div>
+        <div class="col">
+            <img src="orquideas.jpg" width="280" height="190" />
+            <p>Orquídeas</p>
+        </div>
+        <div class="col">
+            <img src="hortensias.jpg" width="280" height="190" />
+            <p>Hortensias</p>
+        </div>
+        <div class="col">
+            <img src="margaritas.jpg" width="295" height="190" />
+            <p>Margaritas</p>
+        </div>
+        <div class="col">
+            <img src="tulipanes.jpg" width="295" height="190" />
+            <p>Tulipanes</p>
+        </div>
+        <div class="col">
+            <img src="amapolas.jpg" width="295" height="190" />
+            <p>Amapola</p>
+        </div>
+        <div class="col">
+            <img src="narcisos.jpg" width="295" height="190" />
+            <p>Narciso</p>
+        </div>
+        <div class="col">
+            <img src="dalia.jpg" width="295" height="190" />
+            <p>Dalia</p>
+        </div>
+    </div>
+    </div>
+    <video width="720" height="480" controls style="float: right;">
+        <source src="rose.mp4" type="video/mp4">
+    </video>
+</I><br>
+<a id="3"></a>
+<h1>Flores medicinales</h1>
+<p>
+    <li>LAVANDA: Son muy variados: alivia la ansiedad y el estrés, mejora el estado de ánimo, aclara el acné, etc.<br>
+        <p>
+    <li> CLAVEL:
+        Cuando se separan de la base de la flor (que es amarga), los pétalos se pueden preparar para hacer un té
+        excelente para reducir la ansiedad, la agitación, el estrés y la fatiga.</li>
+<P>
+    <li>CALÉNDULA:
+        Posee propiedades antiinflamatorias, antifúngicas y antimicrobianas. Es la gran planta medicinal para el cuidado
+        de la piel, por lo que se incluye en muchas pomadas y cremas.
+    </li>
+<p>
+    <li>
+        EQUINÁCEA:
+        Destaca su efecto estimulante sobre el sistema inmunitario que ayuda a prevenir y tratar infecciones víricas del
+        sistema respiratorio.
+        Posee compuestos que han demostrado ayudar a aliviar la ansiedad, reducir la inflamación y mejorar el acné y el
+        eccema.
+    </li>
+<p>
+    <li>
+        MARGARITA:
+        Esta flor tiene una gran cantidad de propiedades medicinales a pesar de su apariencia sencilla. Cuando se
+        consume en infusión, actúa como laxante y expectorante.
+        También se utiliza como remedio casero para ayudar a tratar trastornos como la artritis.
+    </li>
+<p>
+    <li>
+        CRISANTEMO:
+        Los crisantemos pueden sumergirse en agua caliente durante 4 minutos para preparar una infusión de delicioso
+        sabor.
+        Beberla alivia la fiebre, el dolor de cabeza y el resfriado común. </li>
+<p>
+    <li>
+        JAZMÍN:
+
+        Las dulces flores de jazmín no solo hacen una deliciosa taza de té, sino que también ayudan en problemas
+        digestivos y úlceras estomacales.
+        Beber esta infusión antes de acostarse puede ayudar a evitar el insomnio y la ansiedad. </li>
+    <br>
+    <a id="4"></a>
+<h1>¿Sabías que ... ? </h1>
+<br>
+<div class="row">
+    <div class="col">
+        <img src="coliflor.jpg" width="275" height="190">
+        <p>
+            Aunque probablemente no las llegarás a ver en un arreglo floral; la coliflor y el brócoli son flores.
+            Ambos pertenecen a la familia Brassicaceae.
+        </p>
+    </div>
+    <div class="col">
+        <img src="Sunflower.jpg" width="275" height="190">
+        <p>
+            Los girasoles reciben este nombre ya que, literalmente,
+            giran sus tallos y flores hacia los rayos del sol.
+        </p>
+    </div>
+    <div class="col">
+        <img src="cosmos.jpg" width="275" height="190">
+        <p>
+            En México se encuentra una flor de pétalos rojos e interior marrón conocida
+            como el cosmos encarnado o flor de chocolate, y tal como lo indica su nombre, </li>
+            su aroma es igual al del chocolate.
+        </p>
+    </div>
+    <footer>
+        <p style="text-align: center;">&copy; Flores 2023 - Beth. zS20020310@estudiantes.uv.mx </p>
+    </footer>
+    </body>
+
+</html>
